79835e1a79c18b5885b0ea386915625943380472
Till Max Schwikal <schwikal@mailbox.org>
2019-01-06 13:20:41 +0100

    Rename to .ino file

--- WS2801_Arduino_BitBanger.ino
+++ WS2801_Arduino_BitBanger.ino
@@ -1,11 +1,11 @@
-#include "Adafruit_WS2801.h"
+#include <Adafruit_WS2801.h>
 #include "SPI.h" // Comment out this line if using Trinket or Gemma
 #ifdef __AVR_ATtiny85__
- #include <avr/power.h>
+#include <avr/power.h>
 #endif
 
 /*****************************************************************************
-Example sketch for driving Adafruit WS2801 pixels!
+  Example sketch for driving Adafruit WS2801 pixels!
 
 
   Designed specifically to work with the Adafruit RGB Pixels!
@@ -17,11 +17,11 @@ Example sketch for driving Adafruit WS2801 pixels!
   high speed PWM drivers for 24 bit color per pixel
   2 pins are required to interface
 
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
   products from Adafruit!
 
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  Written by Limor Fried/Ladyada for Adafruit Industries.
   BSD license, all text above must be included in any redistribution
 
 *****************************************************************************/
@@ -30,29 +30,22 @@ Example sketch for driving Adafruit WS2801 pixels!
 // Can be any valid output pins.
 // The colors of the wires may be totally different so
 // BE SURE TO CHECK YOUR PIXELS TO SEE WHICH WIRES TO USE!
-uint8_t dataPin  = 2;    // Yellow wire on Adafruit Pixels
-uint8_t clockPin = 3;    // Green wire on Adafruit Pixels
+uint8_t dataPin  = 11;    // Green wire on China PixelStripe ==> MOSI D11
+uint8_t clockPin = 13;    // Blue wire on China PixelStripe ==> SCK D13
 
 // Don't forget to connect the ground wire to Arduino ground,
 // and the +5V wire to a +5V supply
 
-// Set the first variable to the NUMBER of pixels. 25 = 25 pixels in a row
-Adafruit_WS2801 strip = Adafruit_WS2801(25, dataPin, clockPin);
+// Set the first variable to the NUMBER of pixels. Number of pixels in a row
+const int INITIAL_NUM_LED = 200;
+//Adafruit_WS2801 strip = Adafruit_WS2801(INITIAL_NUM_LED, dataPin, clockPin);
 
 // Optional: leave off pin numbers to use hardware SPI
 // (pinout is then specific to each board and can't be changed)
-//Adafruit_WS2801 strip = Adafruit_WS2801(25);
-
-// For 36mm LED pixels: these pixels internally represent color in a
-// different format.  Either of the above constructors can accept an
-// optional extra parameter: WS2801_RGB is 'conventional' RGB order
-// WS2801_GRB is the GRB order required by the 36mm pixels.  Other
-// than this parameter, your code does not need to do anything different;
-// the library will handle the format change.  Examples:
-//Adafruit_WS2801 strip = Adafruit_WS2801(25, dataPin, clockPin, WS2801_GRB);
-//Adafruit_WS2801 strip = Adafruit_WS2801(25, WS2801_GRB);
+Adafruit_WS2801 strip = Adafruit_WS2801(INITIAL_NUM_LED);
 
 void setup() {
+  Serial.begin(9600);
 #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000L)
   clock_prescale_set(clock_div_1); // Enable 16 MHz on Trinket
 #endif
@@ -61,44 +54,49 @@ void setup() {
 
   // Update LED contents, to start they are all 'off'
   strip.show();
+  demo();
 }
 
-
 void loop() {
-  // Some example procedures showing how to display to the pixels
   
-  colorWipe(Color(255, 0, 0), 50);
-  colorWipe(Color(0, 255, 0), 50);
-  colorWipe(Color(0, 0, 255), 50);
-  rainbow(20);
-  rainbowCycle(20);
+  rainbow(100);
+}
+
+void demo() {
+  const int waitTime = 0;
+  // Some example procedures showing how to display to the pixels
+  colorWipe(Color(255, 0, 0), waitTime);
+  colorWipe(Color(0, 255, 0), waitTime);
+  colorWipe(Color(0, 0, 255), waitTime);
+  rainbow(waitTime);
+  rainbowCycle(waitTime);
 }
 
 void rainbow(uint8_t wait) {
   int i, j;
-   
-  for (j=0; j < 256; j++) {     // 3 cycles of all 256 colors in the wheel
-    for (i=0; i < strip.numPixels(); i++) {
+
+  for (j = 0; j < 256; j++) {   // 3 cycles of all 256 colors in the wheel
+    for (i = 0; i < strip.numPixels(); i++) {
       strip.setPixelColor(i, Wheel( (i + j) % 255));
-    }  
+    }
     strip.show();   // write all the pixels out
     delay(wait);
   }
 }
 
-// Slightly different, this one makes the rainbow wheel equally distributed 
+// Slightly different, this one makes the rainbow wheel equally distributed
 // along the chain
 void rainbowCycle(uint8_t wait) {
   int i, j;
-  
-  for (j=0; j < 256 * 5; j++) {     // 5 cycles of all 25 colors in the wheel
-    for (i=0; i < strip.numPixels(); i++) {
+
+  for (j = 0; j < 256 * 5; j++) {   // 5 cycles of all 25 colors in the wheel
+    for (i = 0; i < strip.numPixels(); i++) {
       // tricky math! we use each pixel as a fraction of the full 96-color wheel
       // (thats the i / strip.numPixels() part)
       // Then add in j which makes the colors go around per pixel
       // the % 96 is to make the wheel cycle around
       strip.setPixelColor(i, Wheel( ((i * 256 / strip.numPixels()) + j) % 256) );
-    }  
+    }
     strip.show();   // write all the pixels out
     delay(wait);
   }
@@ -108,11 +106,11 @@ void rainbowCycle(uint8_t wait) {
 // good for testing purposes
 void colorWipe(uint32_t c, uint8_t wait) {
   int i;
-  
-  for (i=0; i < strip.numPixels(); i++) {
-      strip.setPixelColor(i, c);
-      strip.show();
-      delay(wait);
+
+  for (i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
   }
 }
 
@@ -135,12 +133,13 @@ uint32_t Color(byte r, byte g, byte b)
 uint32_t Wheel(byte WheelPos)
 {
   if (WheelPos < 85) {
-   return Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+    return Color(WheelPos * 3, 255 - WheelPos * 3, 0);
   } else if (WheelPos < 170) {
-   WheelPos -= 85;
-   return Color(255 - WheelPos * 3, 0, WheelPos * 3);
+    WheelPos -= 85;
+    return Color(255 - WheelPos * 3, 0, WheelPos * 3);
   } else {
-   WheelPos -= 170; 
-   return Color(0, WheelPos * 3, 255 - WheelPos * 3);
+    WheelPos -= 170;
+    return Color(0, WheelPos * 3, 255 - WheelPos * 3);
   }
 }
+
