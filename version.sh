f5413ff494535e874e5248978c5528c9298b748c
Till Max Schwikal <schwikal@mailbox.org>
2019-02-17 16:15:14 +0100

    Add color format strings to version line

--- version.sh
+++ version.sh
@@ -2,12 +2,18 @@
 
 set -e
 
+# color codes
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+LIGHT_BLUE='\033[1;34m'
+NC='\033[0m' # No Color
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 BINARY=WS2801_Arduino_BitBanger
 VERSION=`git describe --always --dirty`
 DATE=`date -u +%FT%T%z`
-echo "${BINARY}: compiled at ${DATE} with version ${VERSION}"
+echo -e "${GREEN}${BINARY}${NC}: compiled at ${BLUE}${DATE}${NC} with version ${LIGHT_BLUE}${VERSION}${NC}"
 
 echo "// THIS FILE IS AUTO-GENERATED - DO NOT EDIT MANUALLY
 
