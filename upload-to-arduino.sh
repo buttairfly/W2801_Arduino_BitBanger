1d85d200912600abf076e0894ea52a810bd1adab
Till Max Schwikal <8588522+buttairfly@users.noreply.github.com>
2021-03-21 13:52:51 +0100

    upgrade the upload process

--- upload-to-arduino.sh
+++ upload-to-arduino.sh
@@ -2,19 +2,22 @@
 
 set -e
 
+ARDUINO_TYPE="arduino:avr:nano"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+BUILD_DIR="${DIR}/build/${ARDUINO_TYPE//:/.}"
 
 git pull
 
 source ${DIR}/version.sh
 
 arduino-cli compile \
-  --fqbn arduino:avr:nano \
+  --fqbn ${ARDUINO_TYPE} \
   --format json \
+  --export-binaries \
   ${DIR}
 arduino-cli upload  \
-  --fqbn arduino:avr:nano:cpu=atmega328old \
+  --fqbn ${ARDUINO_TYPE}:cpu=atmega328old \
   --port /dev/ttyUSB0 \
-  --input-file ${DIR}/WS2801_Arduino_BitBanger.arduino.avr.nano.hex \
+  --input-file ${BUILD_DIR}/WS2801_Arduino_BitBanger.ino.hex \
   --format json \
   --verify
